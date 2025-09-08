f2d2381c4ad089f87d4db166bd444b22c70f3f00
fabyful <fabyfulgenzi@gmail.com>
2025-09-07 21:14:42 -0300

    cambios en main  del factory method descomentado de pedidos

--- Trabajo_propio/factory/factory_method/main.py
+++ Trabajo_propio/factory/factory_method/main.py
@@ -3,8 +3,8 @@ from .store import NYPizzaStore, ChicagoPizzaStore
 def main():
 
     ny = NYPizzaStore(); chi = ChicagoPizzaStore()
-#    p1 = ny.order_pizza("cheese"); print("Ethan ordered:", p1); print("-----------")
-#S    p2 = chi.order_pizza("cheese"); print("Joel ordered:", p2); print("-----------")
+    p1 = ny.order_pizza("cheese"); print("Ethan ordered:", p1); print("-----------")
+    p2 = chi.order_pizza("cheese"); print("Joel ordered:", p2); print("-----------")
     p3 = ny.order_pizza("veggie"); print("Ana ordered:", p3); print("-----------")
     p4 = chi.order_pizza("veggie"); print("Juliet ordered:", p4); print("-----------")
     p5 = chi.order_pizza("pepperoni"); print("Caroline ordered:", p5); print("-----------")
