47f63c30785347d4c50a49a5d83926718683c9c2
Gerardo1909 <gerardotoboso1909@gmail.com>
2025-09-10 17:08:27 -0300

    refactor(trabajo_propio/factory): Se mejora el formato del código.

--- Trabajo_propio/factory/abstract_factory/ingredients.py
+++ Trabajo_propio/factory/abstract_factory/ingredients.py
@@ -1,68 +1,215 @@
+"""
+Módulo que contiene las clases de ingredientes para las pizzas.
+"""
+
 from abc import ABC, abstractmethod
 
-# Ingredient products
-class Dough:    
-    def __init__(self, name): self.name=name;  
-    def __str__(self): return self.name
-class Sauce:    
-    def __init__(self, name): self.name=name;  
-    def __str__(self): return self.name
-class Cheese:   
-    def __init__(self, name): self.name=name;  
-    def __str__(self): return self.name
-class VeggieCheese:    
-    def __init__(self, name): self.name=name;  
-    def __str__(self): return self.name
-class Clams:    
-    def __init__(self, name): self.name=name;  
-    def __str__(self): return self.name
-class Onion:    
-    def __init__(self, name): self.name=name;  
-    def __str__(self): return self.name
-class Mushroom:    
-    def __init__(self, name): self.name=name;  
-    def __str__(self): return self.name
-class Pepperoni:    
-    def __init__(self, name): self.name=name;  
-    def __str__(self): return self.name
 
-# Abstract Factory
+class Dough:
+    """
+    Clase que representa la masa de la pizza.
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Sauce:
+    """
+    Clase que representa la salsa de la pizza.
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Cheese:
+    """
+    Clase que representa el queso de la pizza.
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class VeggieCheese:
+    """
+    Clase que representa el queso vegetariano de la pizza.
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Clams:
+    """
+    Clase que representa las almejas de la pizza.
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Onion:
+    """
+    Clase que representa la cebolla de la pizza.
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Mushroom:
+    """
+    Clase que representa el champiñón de la pizza.
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Pepperoni:
+    """
+    Clase que representa el pepperoni de la pizza.
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
 class PizzaIngredientFactory(ABC):
+    """
+    Interfaz para la creación de ingredientes de pizza.
+    """
+
     @abstractmethod
-    def create_dough(self) -> Dough: ...
+    def create_dough(self) -> Dough:
+        """
+        Añade la masa de la pizza.
+        """
+
     @abstractmethod
-    def create_sauce(self) -> Sauce: ...
+    def create_sauce(self) -> Sauce:
+        """
+        Añade la salsa de la pizza.
+        """
+
     @abstractmethod
-    def create_cheese(self) -> Cheese: ...
+    def create_cheese(self) -> Cheese:
+        """
+        Añade el queso de la pizza.
+        """
+
     @abstractmethod
-    def create_veggiecheese(self) -> VeggieCheese: ...
+    def create_veggiecheese(self) -> VeggieCheese:
+        """
+        Añade el queso vegetariano de la pizza.
+        """
+
     @abstractmethod
-    def create_clam(self) -> Clams: ...
+    def create_clam(self) -> Clams:
+        """
+        Añade las almejas de la pizza.
+        """
+
     @abstractmethod
-    def create_onion(self) -> Onion: ...
+    def create_onion(self) -> Onion:
+        """
+        Añade la cebolla de la pizza.
+        """
+
     @abstractmethod
-    def create_mushroom(self) -> Mushroom: ...
-    #@abstractmethod
-    def create_pepperoni(self) ->  Pepperoni:   return Pepperoni("Sliced Pepperoni") 
-    
-# Concrete factories
+    def create_mushroom(self) -> Mushroom:
+        """
+        Añade el champiñón de la pizza.
+        """
+
+    @abstractmethod
+    def create_pepperoni(self) -> Pepperoni:
+        """
+        Añade el pepperoni de la pizza.
+        """
+
+
 class NYPizzaIngredientFactory(PizzaIngredientFactory):
-    def create_dough(self) -> Dough:  return Dough("Thin Crust Dough")
-    def create_sauce(self) -> Sauce:  return Sauce("Marinara Sauce")
-    def create_cheese(self) -> Cheese:return Cheese("Reggiano Cheese")
-    def create_onion(self) -> Onion:   return Onion("Caramelized Sliced Onion")    
-    def create_veggiecheese(self) -> Cheese:return VeggieCheese("Veggie Cheese")
-    def create_clam(self) -> Clams:   return Clams("Fresh Clams")
-    def create_mushroom(self) -> Mushroom:   return Mushroom("Sliced Girgolas")    
-#    def create_SlicedPepperoni(self) -> Pepperoni:   return Pepperoni("Sliced Pepperoni")   
+    """
+    Clase concreta para la creación de ingredientes de pizza estilo NY.
+    """
+
+    def create_dough(self) -> Dough:
+        return Dough("Thin Crust Dough")
+
+    def create_sauce(self) -> Sauce:
+        return Sauce("Marinara Sauce")
+
+    def create_cheese(self) -> Cheese:
+        return Cheese("Reggiano Cheese")
+
+    def create_onion(self) -> Onion:
+        return Onion("Caramelized Sliced Onion")
+
+    def create_veggiecheese(self) -> VeggieCheese:
+        return VeggieCheese("Veggie Cheese")
+
+    def create_clam(self) -> Clams:
+        return Clams("Fresh Clams")
+
+    def create_mushroom(self) -> Mushroom:
+        return Mushroom("Sliced Girgolas")
+
+    def create_pepperoni(self) -> Pepperoni:
+        return Pepperoni("Sliced Pepperoni")
 
 
 class ChicagoPizzaIngredientFactory(PizzaIngredientFactory):
-    def create_dough(self) -> Dough:  return Dough("Thick Crust Dough")
-    def create_sauce(self) -> Sauce:  return Sauce("Plum Tomato Sauce")
-    def create_cheese(self) -> Cheese:return Cheese("Mozzarella Cheese")
-    def create_onion(self) -> Onion:   return Onion("Red Caramelized Sliced Onion")    
-    def create_veggiecheese(self) -> Cheese:return Cheese("Veggie Cheese")
-    def create_clam(self) -> Clams:   return Clams("Frozen Clams")
-    def create_mushroom(self) -> Mushroom:   return Mushroom("Sliced mushroom")    
-#    def create_SlicedPepperoni(self) -> Pepperoni:   return Pepperoni("Sliced Pepperoni")  
+    """
+    Clase concreta para la creación de ingredientes de pizza estilo Chicago.
+    """
+
+    def create_dough(self) -> Dough:
+        return Dough("Thick Crust Dough")
+
+    def create_sauce(self) -> Sauce:
+        return Sauce("Plum Tomato Sauce")
+
+    def create_cheese(self) -> Cheese:
+        return Cheese("Mozzarella Cheese")
+
+    def create_onion(self) -> Onion:
+        return Onion("Red Caramelized Sliced Onion")
+
+    def create_veggiecheese(self) -> VeggieCheese:
+        return VeggieCheese("Veggie Cheese")
+
+    def create_clam(self) -> Clams:
+        return Clams("Frozen Clams")
+
+    def create_mushroom(self) -> Mushroom:
+        return Mushroom("Sliced mushroom")
+
+    def create_pepperoni(self) -> Pepperoni:
+        return Pepperoni("Sliced Pepperoni")
--- Trabajo_propio/factory/factory_method/store.py
+++ Trabajo_propio/factory/factory_method/store.py
@@ -1,25 +1,64 @@
+"""
+Código que representa tiendas de pizzas.
+"""
+
 from abc import ABC, abstractmethod
-from .pizza import *#Pizza, NYStyleCheesePizza, ChicagoStyleCheesePizza
+from pizza import (
+    NYStyleCheesePizza,
+    NYStyleVeggiePizza,
+    ChicagoStyleCheesePizza,
+    ChicagoStyleVeggiePizza,
+    NYStylePepperoniPizza,
+    ChicagoStylePepperoniPizza,
+    Pizza,
+)
+
 
 class PizzaStore(ABC):
+    """
+    Clase que representa una tienda de pizzas.
+    """
     def order_pizza(self, kind: str) -> Pizza:
+        """
+        Procesa un pedido de pizza.
+        """
         pizza = self.create_pizza(kind)
-        pizza.prepare(); pizza.bake(); pizza.cut(); pizza.box()
+        pizza.prepare()
+        pizza.bake()
+        pizza.cut()
+        pizza.box()
         return pizza
+
     @abstractmethod
-    def create_pizza(self, kind: str) -> Pizza: ...
+    def create_pizza(self, kind: str) -> Pizza:
+        """
+        Crea una pizza del tipo especificado.
+        """
+
 
 class NYPizzaStore(PizzaStore):
+    """
+    Clase que representa una tienda de pizzas estilo NY.
+    """
     def create_pizza(self, kind: str) -> Pizza:
-        
-        if kind.lower() == "cheese": return NYStyleCheesePizza()
-        if kind.lower() == "veggie": return NYStyleVeggiePizza()
-        if kind.lower() == "pepperoni": return NYStyleVeggiePizza()
+        if kind.lower() == "cheese":
+            return NYStyleCheesePizza()
+        if kind.lower() == "veggie":
+            return NYStyleVeggiePizza()
+        if kind.lower() == "pepperoni":
+            return NYStylePepperoniPizza()
         raise ValueError(f"No NY pizza for kind: {kind}")
 
+
 class ChicagoPizzaStore(PizzaStore):
+    """
+    Clase que representa una tienda de pizzas estilo Chicago.
+    """
     def create_pizza(self, kind: str) -> Pizza:
-        if kind.lower() == "cheese": return ChicagoStyleCheesePizza()
-        if kind.lower() == "veggie": return ChicagoStyleVeggiePizza()
-        if kind.lower() == "pepperoni": return ChicagoStyleVeggiePizza()
+        if kind.lower() == "cheese":
+            return ChicagoStyleCheesePizza()
+        if kind.lower() == "veggie":
+            return ChicagoStyleVeggiePizza()
+        if kind.lower() == "pepperoni":
+            return ChicagoStylePepperoniPizza()
         raise ValueError(f"No Chicago pizza for kind: {kind}")
